747242bda0cf6d81ebab3857c8b2a26475eeb51d
Tim Stawowski <t.stawowski@wertgarantie.com>
2020-07-07 21:49:42 +0200

    Add hex
    Add license
    
    Add openapi file

--- mix.exs
+++ mix.exs
@@ -5,26 +5,35 @@ defmodule CainOpenApi.MixProject do
     [
       app: :cain_oa,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "CainOpenApi",
+      source_url: "https://github.com/timstawowski/cain_oa"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:jason, ">= 1.0.0"}
+    ]
+  end
 
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+  defp description do
+    "Compile time interpreter of Camunda's OpenAPI specification"
+  end
+
+  defp package do
+    [
+      name: "CainOpenApi",
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/timstawowski/cain_oa"}
     ]
   end
 end
