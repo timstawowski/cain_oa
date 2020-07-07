d3499dea5fe1a3a6901a096c9ff6e454f9980f30
Tim Stawowski <t.stawowski@wertgarantie.com>
2020-07-07 23:28:22 +0200

    Upgrade required elixir v

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CainOpenApi.MixProject do
     [
       app: :cain_oa,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
